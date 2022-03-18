759a7b64ea5e3bb1ae352524d57deeeecc01e283
Vitor Rodrigues <vitor.felix@cwvdigital.com.br>
2022-03-18 18:48:14 -0300

    Configure API for GET data.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,24 @@ import IndicadorVenda from "./Main/IndicadorVendas/indicadorVenda";
 import IndicadorFaturamento from "./Main/IndicadorFaturamento/indicadorFaturamento";
 import IndicadorProduto from "./Main/IndicadorProdutos/indicadorProduto";
 
+//API
+import api from 'services/api';
+
+//HOOKS
+import { useState, useEffect } from 'react';
+
 const Home = () => {
+
+    //Variáveis de Estado
+    const [indEmissao, setIndEmissao] = useState([]);
+    const [indVenda, setIndVenda] = useState([]);
+    const [indFaturamento, setIndFaturamento] = useState([]);
+    const [indProduto, setIndProduto] = useState([]);
+
+    useEffect(() => {
+        api.get('/pedido');
+    }, []);
+
     return(
         <>
             <section className="container">
