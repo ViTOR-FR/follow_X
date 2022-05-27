f5b027d1aeee90f19d81faa4279916998e2655b9
VITOR FELIX RODRIGUES <vitor.rodrigues@solutinet.com.br>
2022-05-27 17:00:29 -0300

    Correção de arquivo para envio ao Cloud

--- src/pages/Home/Main/IndicadorProdutos/indicadorProduto.js
+++ src/pages/Home/Main/IndicadorProdutos/indicadorProduto.js
@@ -9,7 +9,6 @@ const IndicadorProduto = () => {
                     <div className="mt-1">
                         <p>PJ A1 - 1 Ano Arquivo: <span>0</span></p>
                         <p>PF A1 - 1 Ano Arquivo: <span>0</span></p>
-                        <p>Bird ID - 5 Assinaturas: <span>0</span></p>
                     </div>
                 </div>
             </div>
